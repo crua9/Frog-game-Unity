988891bfe9066cf10da2457df66ade595a729c4a
Craig Bennett II <craigtbii@gmail.com>
2016-09-11 03:31:38 -0400

    move the frog all over, and have the animations
    c# get the frog to move.
    Change a script in player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,43 @@ public class PlayerMovement : MonoBehaviour {
     private float moveHorizontal;
     private float moveVertical;
     private Vector3 movement;
+    private float turningSpeed = 20f;
+    private Rigidbody playerRidgidbody;
 
     // Use this for initialization
     void Start()
     {
-
+        // Gather the animator compoent from the player game object
+        playerAnimator = GetComponent<Animator>();
+        playerRidgidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Gather input from the keyboard
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
 
         movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    }
+    void FixedUpdate()
+    {
+        //If the player is moving (the vector doesn't = 0)
+        if(movement != Vector3.zero) {
+            // a target rotation based on the movement vector
+            Quaternion targetRotation = Quaternion.LookRotation(movement, Vector3.up);
+            Quaternion newRootation = Quaternion.Lerp(playerRidgidbody.rotation, targetRotation, turningSpeed * Time.deltaTime);
+
+            // change the players rotation to the new incremental rotation
+            playerRidgidbody.MoveRotation(newRootation);
+            //play jump animation
+            playerAnimator.SetFloat("Speed", 3f);
+        }
+        else
+        {
+            //don't play it when they aren't
+            playerAnimator.SetFloat("Speed", 0f);
+        }
     }
 }
