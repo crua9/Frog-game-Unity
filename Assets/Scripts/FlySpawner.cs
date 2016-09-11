0d60a762153f4933d4f813ecb38f46c2258b1656
Craig Bennett II <craigtbii@gmail.com>
2016-09-11 19:37:53 -0400

    Fly Spawner 1

---
+++ Assets/Scripts/FlySpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlySpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject flyPrefab;
+    [SerializeField]
+    private int totalFlyminium = 12;
+    
+    private float spawnArea = 25f;
+    public static int totalFlies;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
