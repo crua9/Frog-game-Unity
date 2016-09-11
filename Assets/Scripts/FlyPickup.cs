1267888bbb0dedf006e88c674e5aa2578f37798c
Craig Bennett II <craigtbii@gmail.com>
2016-09-11 19:25:49 -0400

    Fly goes away
    When player touches the fly, it goes away

---
+++ Assets/Scripts/FlyPickup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyPickup : MonoBehaviour {
+
+	void onTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
