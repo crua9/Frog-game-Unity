a61aec15dd2c0e41251060b7f9f057af773ab90f
Craig Bennett II <craigtbii@gmail.com>
2016-09-11 19:31:23 -0400

    Pickup Particles
    What happens when you touch the fly, it goes away, and it has some
    particle effect

--- Assets/Scripts/FlyPickup.cs
+++ Assets/Scripts/FlyPickup.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 
 public class FlyPickup : MonoBehaviour {
-
+    [SerializeField]
+    private GameObject pickupPrefab;
 	void onTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Instantiate(pickupPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
