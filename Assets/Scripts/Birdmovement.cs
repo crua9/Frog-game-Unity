733a5f9f55bf714fba5c05a92e3e486407ec5641
Craig Bennett II <craigtbii@gmail.com>
2016-09-12 18:26:26 -0400

    Bird goes after frog and score works
    The bird goes after the bird, but doesn't do anything when it gets to
    it. Also, the score system works

---
+++ Assets/Scripts/Birdmovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Birdmovement : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+    private NavMeshAgent birdAgent;
+    private Animator birdAnimator;
+
+	// Use this for initialization
+	void Start () {
+        birdAgent = GetComponent<NavMeshAgent>();
+        birdAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        birdAgent.SetDestination(target.position);
+
+        float speed = birdAgent.velocity.magnitude;
+
+        birdAnimator.SetFloat("Speed", speed);
+	}
+}
