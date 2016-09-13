4c98b05e74dd1236521ea01394271e78dc07ba61
Craig Bennett II <craigtbii@gmail.com>
2016-09-12 21:06:02 -0400

    Started on the sounds
    I started on the sounds. It's getting too dark to see what I'm doing

---
+++ Assets/Scripts/RandomSoundPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomSoundPlayer : MonoBehaviour {
+
+    private AudioSource audiosource;
+    [SerializeField]
+    private List<AudioClip> soundClips = new List<AudioClip>();
+    [SerializeField]
+    private float soundTimerDelay = 3f;
+    private float soundtimer;
+    // Use this for initialization
+    void Start () {
+        audiosource = GetHashCode<audiosource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //May need to work on this line. It was too dark for me to really tell what I am typing.
+        soundtimer = soundtimer + Time.deltaTime;
+	}
+}
